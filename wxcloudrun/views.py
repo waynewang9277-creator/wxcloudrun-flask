6cdb6987405097654eb515bf0dee3699f5a1c805
waynewang9277-creator <waynewang9277@gmail.com>
2026-04-19 18:09:38 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, jsonify
-from .response import make_response
-
 count_bp = Blueprint('count', __name__)
-
+def make_json_response(data):
+    return jsonify({'code': 0, 'data': data, 'msg': 'success'})
 @count_bp.route('/api/count', methods=['GET'])
 def get_count():
     from .model import get_count
-    return make_response(data={'count': get_count()})
-
+    return make_json_response({'count': get_count()})
 @count_bp.route('/api/count', methods=['POST'])
 def update_count():
     from flask import request
@@ -17,4 +15,4 @@ def update_count():
         count_up()
     elif action == 'clear':
         count_reset()
-    return make_response(data={'count': get_count()})
+    return make_json_response({'count': get_count()})
