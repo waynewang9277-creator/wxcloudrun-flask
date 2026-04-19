dcb47dc33aa7e5f41c8e95d219055502bca6811d
waynewang9277-creator <waynewang9277@gmail.com>
2026-04-19 22:17:06 +0800

    Simplify views to remove model dependency

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -9,17 +9,9 @@ def make_json_response(data):
 
 @count_bp.route('/api/count', methods=['GET'])
 def get_count():
-    from .model import get_count
-    return make_json_response({'count': get_count()})
+    return make_json_response({'count': 0})
 
 
 @count_bp.route('/api/count', methods=['POST'])
 def update_count():
-    from flask import request
-    from .model import count_up, count_reset
-    action = request.json.get('action', 'inc')
-    if action == 'inc':
-        count_up()
-    elif action == 'clear':
-        count_reset()
-    return make_json_response({'count': get_count()})
+    return make_json_response({'count': 0})
