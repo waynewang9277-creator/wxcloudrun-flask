263187a85ca707f9f52dd19b4ddd7169a1241cd1
waynewang9277-creator <waynewang9277@gmail.com>
2026-04-19 22:14:08 +0800

    Restore Flask-SQLAlchemy with SQLite (no PyMySQL)

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from wxcloudrun import views
 app.register_blueprint(views.count_bp)
