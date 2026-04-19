d9b3c8d2f9824d94e0e407ad951c43ba5ad453df
waynewang9277-creator <waynewang9277@gmail.com>
2026-04-19 22:09:15 +0800

    Remove battery_test temporarily to test deployment

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,16 +1,10 @@
 from flask import Blueprint, jsonify
 
 count_bp = Blueprint('count', __name__)
-app_bp = Blueprint('app', __name__)
 
 
 def make_json_response(data):
     return jsonify({'code': 0, 'data': data, 'msg': 'success'})
-
-
-# 注册电池测试蓝图
-from .routes.battery_test import battery_test_bp
-app_bp.register_blueprint(battery_test_bp, url_prefix='/api/battery-test')
 
 
 @count_bp.route('/api/count', methods=['GET'])
