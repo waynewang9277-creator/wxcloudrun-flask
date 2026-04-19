aa2d888011f3c79a4ad99675cd40774879981727
waynewang9277-creator <waynewang9277@gmail.com>
2026-04-19 22:10:59 +0800

    Remove Flask-SQLAlchemy and PyMySQL to test bare Flask

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -1,17 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
-import config
 
-pymysql.install_as_MySQLdb()
-
-app = Flask(__name__, instance_relative_config=True)
-app.config['DEBUG'] = config.DEBUG
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/flask_demo'.format(config.username, config.password, config.db_address)
-
-db = SQLAlchemy(app)
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
 from wxcloudrun import views
 app.register_blueprint(views.count_bp)
-
-app.config.from_object('config')
