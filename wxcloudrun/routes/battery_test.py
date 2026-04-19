cd67f7b54b9e4e561f3ca6905cb40dc04f90e5e0
waynewang9277-creator <waynewang9277@gmail.com>
2026-04-19 22:48:35 +0800

    Test reportlab import in battery_test

--- wxcloudrun/routes/battery_test.py
+++ wxcloudrun/routes/battery_test.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, jsonify
 
+# 娴嬭瘯 reportlab import
+try:
+    from reportlab.lib.pagesizes import A4
+    REPORTLAB_OK = True
+except Exception as e:
+    REPORTLAB_OK = str(e)
+
 battery_test_bp = Blueprint('battery_test', __name__)
 
 
 @battery_test_bp.route('/health', methods=['GET'])
 def health():
-    return jsonify({'status': 'ok'})
+    return jsonify({'status': 'ok', 'reportlab': REPORTLAB_OK})
